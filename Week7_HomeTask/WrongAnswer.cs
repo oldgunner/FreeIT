e390c2c26a851ef46d8a557bd38f3b73d9284831
Vitali <vitalihrybko@gmail.com>
2020-10-05 17:26:46 +0300

    Added class WrongAnswer separated from Program class

---
+++ Week7_HomeTask/WrongAnswer.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+
+namespace Week7_HomeTask
+{
+    class WrongAnswer : Answer
+    {
+        public WrongAnswer(string text) : base(text)
+        {
+        }
+
+        public override void CheckAnswers()
+        {
+            Console.WriteLine("{0}Вы ответили неправильно!{0}", Environment.NewLine);
+        }
+    }
+}
